b08881c7afbb17f09290360955d34d06f82ab8fb
MayaPorto <mayamiresporto@gmail.com>
2022-09-17 17:14:57 -0300

    upgrade

---
+++ Questao1/1.js
@@ -0,0 +1,8 @@
+//alert("Código Funcionando")
+
+let base= Number(prompt('Qual é o valor da base?'))
+let altura= Number(prompt('Qual é o valor da altura?'))
+let area= Number(prompt(' O valor da área é: ' +  base * altura + ' e o perímetro é ' + 2*(base + altura)))
+
+
+
